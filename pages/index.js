9c679eb3ba4de4f64e2d4442c3528ab86affc8e4
Daviani <contact@daviani.dev>
2022-11-17 10:55:51 +0100

    add translation

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Layout from '../components/Layout'
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import i18n from '../lib/i18n'
 
 export default function Home () {
-
+  const { locale } = useRouter()
   return (
     <div className="w-screen bg-main-100 dark:bg-main-700 " >
       <Layout >
@@ -12,11 +14,10 @@ export default function Home () {
             <button type="button"
                     className=" m-5 px-4 py-2 text-sm font-semibold leading-6 text-main-50 dark:text-main-50 bg-main-700 dark:bg-main-800 rounded-md"
             >
-              Characters
+              {i18n.homePage.charactersButton[locale]}
             </button >
           </Link >
         </div >
-
       </Layout >
     </div >
   )
